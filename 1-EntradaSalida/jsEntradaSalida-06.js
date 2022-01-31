833d69782343eab769a17c7a1d277c8c5401d5db
BrianNahuelreynoso1995 <98592334+BrianNahuelreynoso1995@users.noreply.github.com>
2022-01-31 19:29:29 -0300

    ejercicios del 1 al 6

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,30 @@
 /*
+brian reynoso
+E/S ejercicio 6
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	//creo la primer variable
+	var numeroUno = txtIdNumeroUno.value;
+
+	// cre la segunda variable
+	var numeroDos = txtIdNumeroDos.value;
+
+	// pasar el numero de texto a valor
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = parseInt(numeroDos);
+
+	// se suman las variables
+	resultado = numeroUno + numeroDos;
+
+	alert("Su suma es " + resultado );
 }
 
+
+
+
+// txtIdNumeroUno
+// txtIdNumeroDos
